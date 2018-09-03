8deda606d22371d48f59c2df92587e06fa45820e
山本 沙季 <saki.yamamoto@d2cdot.co.jp>
2018-09-03 20:03:39 +0900

    ロジックの追加

--- main.js
+++ main.js
@@ -4,15 +4,40 @@
   var btn = document.getElementById('btn');
   var arr = [1,2,3,4,5,6,7,8,9,10];
 
+  function loopSleep(_loopLimit,_interval, _mainFunc){
+  var loopLimit = _loopLimit;
+  var interval = _interval;
+  var mainFunc = _mainFunc;
+  var i = 0;
+  var loopFunc = function () {
+    var result = mainFunc(i);
+    if (result === false) {
+      // break機能
+      return;
+    }
+    i = i + 1;
+    if (i < loopLimit) {
+      setTimeout(loopFunc, interval);
+    }
+  }
+  loopFunc();
+}
+
+
   btn.addEventListener('click', function() {
+
+    loopSleep(10, 1000, function(i){
+      console.log(i);
+    });
+
     if(arr.length == 0){
       this.textContent = "終わり";
     }else{
       var n = Math.floor(Math.random()* arr.length);
       this.textContent = arr[n];
       arr.splice(n,1);
-      console.log( arr.length);
     }
+
 
 
 
