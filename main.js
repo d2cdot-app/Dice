86b708cdeef0ade720d0c4dc3542fea405ce499a
山本 沙季 <saki.yamamoto@d2cdot.co.jp>
2018-09-19 19:10:21 +0900

    no message

--- main.js
+++ main.js
@@ -4,47 +4,34 @@
   var btn = document.getElementById('btn');
   var arr = [1,2,3,4,5,6,7,8,9,10];
 
-  function loopSleep(_loopLimit,_interval, _mainFunc){
-  var loopLimit = _loopLimit;
-  var interval = _interval;
-  var mainFunc = _mainFunc;
-  var i = 0;
-  var loopFunc = function () {
-    var result = mainFunc(i);
-    if (result === false) {
-      // break機能
-      return;
-    }
-    i = i + 1;
-    if (i < loopLimit) {
-      setTimeout(loopFunc, interval);
-    }
-  }
-  loopFunc();
-}
+
+    btn.addEventListener('click', function() {
+      var count = 0;
+      var this_btn = this;
+      var countup = function(){
+        var id = setTimeout(countup,300);
+        this_btn.textContent = count++;
+        if(count > 5){
+          clearTimeout(id);
+          if(arr.length == 0){
+            this_btn.textContent = "終わり";
+          }else{
+            console.log("test1");
+            var n = Math.floor(Math.random()* arr.length);
+            this_btn.textContent = arr[n];
+            arr.splice(n,1);
+
+          }
+        }
+      }
+      countup();
 
 
-  btn.addEventListener('click', function() {
-    var this_btn = this;
-    loopSleep(10, 100, function(i){
-      console.log(this_btn);
-      this_btn.textContent = i;
     });
-
-    if(arr.length == 0){
-      this.textContent = "終わり";
-    }else{
-      var n = Math.floor(Math.random()* arr.length);
-      this.textContent = arr[n];
-      arr.splice(n,1);
-      console.log(n);
-
-    }
-  });
-  btn.addEventListener('mousedown', function() {
-    this.className = 'pushed';
-  });
-  btn.addEventListener('mouseup', function() {
-    this.className = '';
-  });
-})();
+    btn.addEventListener('mousedown', function() {
+      this.className = 'pushed';
+    });
+    btn.addEventListener('mouseup', function() {
+      this.className = '';
+    });
+  })();
