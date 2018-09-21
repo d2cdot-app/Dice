9cd29ccddcc40478d4adfc30ece762b3dea59ef3
山本 沙季 <saki.yamamoto@d2cdot.co.jp>
2018-09-21 15:11:14 +0900

    no message

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
   'use strict';
 
   var btn = document.getElementById('btn');
-  var arr = [1,2,3,4,5,6,7,8,9,10];
+  var array01 = [1,2,3,4,5,6,7,8,9,10];
+  var array02 = [];
 
 
     btn.addEventListener('click', function() {
@@ -10,20 +11,33 @@
       var this_btn = this;
       var countup = function(){
       var id = setTimeout(countup,300);
-        this_btn.textContent = Math.floor(Math.random()* arr.length);
-        console.log(count);
-        count++;
-        if(arr.length == 0){
-          this_btn.textContent = "終わり";
+      var a = Math.random();
+      var n = Math.floor( a * array01.length);
+      this_btn.textContent = array01[n];
+      array02.push(array01[n]);
+      array01.splice(n,1);
+          console.log("this_btn.textContent" + this_btn.textContent );
+          // console.log("Math.random()" + a);
+          // console.log("array01.length" + array01.length);
+          console.log(count);
+          console.log("a"+ array01);
+          console.log("b"+ array02);
+          count++;
+
+        if(array01.length == 0 && array02.length == 0){
           clearTimeout(id);
-        }else if(arr.length == count){
+          console.log("test2");
+          this_btn.textContent = "終わり";
+        }else if(array01.length == 0){
           clearTimeout(id);
           console.log("test1");
-          var n = Math.floor(Math.random()* arr.length);
-          this_btn.textContent = arr[n];
-          arr.splice(n,1);
-
+          Math.floor(Math.random()* array02.length);
+          this_btn.textContent = array02[n];
+          array02.splice(n,1);
+          array01 = array02;
+          array02 = [];
           }
+
         }
 
       countup();
